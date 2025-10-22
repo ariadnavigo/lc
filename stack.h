18e0ff7bf5df15111349608058d17754fdd38ea5
Ariadna Vigo <ariadna@ariadnavigo.xyz>
2025-10-22 18:32:13 +0200

    stack.h: extern variables go after function decls

--- stack.h
+++ stack.h
@@ -5,10 +5,10 @@
 
 #define STACK_SIZE 32 /* Elements max allowed in stack */
 
-extern double stack[STACK_SIZE];
-extern int sp; /* Our "stack pointer" */
-
 int stack_push(double item);
 int stack_pop(double *dest, ...);
+
+extern double stack[STACK_SIZE];
+extern int sp; /* Our "stack pointer" */
 
 #endif
